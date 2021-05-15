e1f6d74b7f5ccdba4a67e0ab6c903b86a70fd1be
Ben Girardet <ben@platform5.ch>
2021-05-15 20:53:14 +0200

    card: use color from card design system provider

--- packages/web-components/fast-components/src/card/card.styles.ts
+++ packages/web-components/fast-components/src/card/card.styles.ts
@@ -2,6 +2,7 @@ import { css } from "@microsoft/fast-element";
 import { display, forcedColorsStylesheetBehavior } from "@microsoft/fast-foundation";
 import { SystemColors } from "@microsoft/fast-web-utilities";
 import { elevation } from "../styles/index";
+import { neutralForegroundRestBehavior } from "./../styles/recipes";
 
 export const CardStyles = css`
     ${display("block")} :host {
@@ -12,15 +13,18 @@ export const CardStyles = css`
         width: var(--card-width, 100%);
         box-sizing: border-box;
         background: var(--background-color);
+        color: ${neutralForegroundRestBehavior.var};
         border-radius: calc(var(--corner-radius) * 1px);
         ${elevation}
     }
 `.withBehaviors(
+    neutralForegroundRestBehavior,
     forcedColorsStylesheetBehavior(
         css`
             :host {
                 forced-color-adjust: none;
                 background: ${SystemColors.Canvas};
+                color: ${SystemColors.CanvasText};
                 box-shadow: 0 0 0 1px ${SystemColors.CanvasText};
             }
         `
